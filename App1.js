55f78f8f97430355e856f3cffc5b156ebfd7d9e6
Atar <atar.pandey@proptiger.com>
2019-07-30 17:30:34 +0530

    added new file app1

---
+++ App1.js
@@ -0,0 +1,52 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { FlatList, ActivityIndicator, Text, View } from 'react-native';
+
+export default class App1 extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { isLoading: true }
+    }
+
+    componentDidMount() {
+        return fetch('https://facebook.github.io/react-native/movies.json')
+            .then((response) => response.json())
+            .then((responseJson) => {
+
+                this.setState({
+                    isLoading: false,
+                    dataSource: responseJson.movies,
+                }, function () {
+
+                });
+
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+
+
+    render() {
+
+        if (this.state.isLoading) {
+            return (
+                <View style={{ flex: 1, padding: 20 }}>
+                    <ActivityIndicator />
+                </View>
+            )
+        }
+
+        return (
+            <View style={{ flex: 1, paddingTop: 20 }}>
+                <FlatList
+                    data={this.state.dataSource}
+                    renderItem={({ item }) => <Text>{item.title}, {item.releaseYear}</Text>}
+                    keyExtractor={({ id }, index) => id}
+                />
+            </View>
+        );
+    }
+}
--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import App1 from './App1';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => App1);
